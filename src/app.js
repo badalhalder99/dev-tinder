bcc378965d6ebc18dc2efec19b92719992c4cb56
Badal Chandra Halder <badalchandrahalder99@gmail.com>
2025-09-15 23:31:16 +0600

    refactor user routes in app.js for clarity and consistency

--- src/app.js
+++ src/app.js
@@ -4,20 +4,30 @@ require("dotenv").config();
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use("/", (req, res) => {
-  res.send("Hello to homepage")
+app.get("/user", (req, res) => {
+  res.send({firstname: "Badal", lastname: "Halder"})
 })
 
-app.use("/hello", (req, res) => {
-  res.send("Hello from the server 2!")
+app.post("/user", (req, res) => {
+  res.send("Data save successfully to the database!")
 })
 
+app.put("/user", (req, res) => {
+  res.send("Data edited successfully!")
+})
+
+app.delete("/user", (req, res) => {
+  res.send("Data deleted successfully!")
+})
+
+// this will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
   res.send("Hello from the server!")
 })
+
 
 app.listen(PORT, () => {
   console.log(`Server is successfully running on ${PORT}`)
 })
 
-//end of the file.Do next.
+//end of the file.Do next..
