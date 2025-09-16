c2e710557567a9bbbceda3315cfe2a35f41021fa
Badal Chandra Halder <badalchandrahalder99@gmail.com>
2025-09-16 18:18:15 +0600

    creating user schema, model and signup api

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
 const express = require("express")
 require("dotenv").config()
 const { connectDB } = require("./config/database")
-
+const { User } = require("./models/user")
 const app = express()
 const PORT = process.env.PORT || 3000
+
+app.post("/signup", async (req, res) => {
+   const userObj = {
+      firstName: "Rudra",
+      lastName: "Howlader",
+      emailId: "badalchandrahalder99@gmai.com",
+      password: "Badal1234"
+   }
+
+   //creating a new instance of the user model
+   const user = new User(userObj)
+
+   try {
+      await user.save()
+      res.send("User added successfully!")
+   } catch (err) {
+      res.status(400).send(`Error saving the user ${err.message}`)
+   }
+})
 
 connectDB().then(() => {
    console.log("Database connected successfully!")
