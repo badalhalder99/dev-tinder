4cbc734084d66685b9b4f5579589886eed747a5b
Badal Chandra Halder <badalchandrahalder99@gmail.com>
2025-10-04 11:23:09 +0600

    fix: update error handling in signup API to return 500 status code; enhance user schema with required fields and validations

--- src/models/user.js
+++ src/models/user.js
@@ -3,24 +3,44 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
    firstName: {
-      type: String
+      type: String,
+      required: true
    },
    lastName: {
       type: String
    },
    emailId: {
-      type: String
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true
    },
    password: {
       type: String
    },
    age: {
-      type: Number
+      type: Number,
+      min: 18
    },
    gender: {
+      type: String,
+      validate(value) {
+         if (!["male", "female", "others"].includes(value)) {
+            throw new Error("Gender data is not valid!")
+         }
+      }
+   },
+   photoUrl: {
       type: String
+   },
+   about: {
+      type: String,
+      default: "This is default description of about"
+   },
+   skills: {
+      type: [String]
    }
-})
+}, { timestamps: true })
 
 const User = mongoose.model("User", userSchema)
 
